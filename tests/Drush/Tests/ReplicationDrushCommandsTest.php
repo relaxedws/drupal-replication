f5c07e4597289926d7f86aca3bbcc60431ee0c27
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-05-05 15:13:02 +0300

    Tests configuration.

--- tests/Drush/Tests/ReplicationDrushCommandsTest.php
+++ tests/Drush/Tests/ReplicationDrushCommandsTest.php
@@ -7,29 +7,12 @@ if (class_exists('Unish\CommandUnishTestCase')) {
   /*
    * @file
    * PHPUnit Tests for replication. This uses Drush's own test framework, based on PHPUnit.
-   * To run the tests, use run-tests-drush.sh from the replication directory.
    */
   class ReplicationDrushCommandsTest extends CommandUnishTestCase {
 
-    /**
-     * Default idle timeout for commands.
-     *
-     * @var int
-     */
-    private $defaultIdleTimeout = 120;
-
-    /**
-     * Idle timeouts for commands.
-     *
-     * Reset to $defaultIdleTimeout after executing a command.
-     *
-     * @var int
-     */
-    protected $idleTimeout = 120;
-
     public function testReplicationCommands() {
       // Specify '8' just in case user has not set UNISH_DRUPAL_MAJOR_VERSION env variable.
-      $sites = $this->setUpDrupal(1, TRUE, '8', 'standard');
+      $sites = $this->setUpDrupal(1, TRUE, '8', 'minimal');
       $target = dirname(__DIR__);
       \symlink($target, $this->webroot() . '/modules/replication');
       \symlink($target . '/../../replication.drush.inc', UNISH_SANDBOX . '/share/drush/commands/replication.drush.inc');
@@ -49,9 +32,32 @@ if (class_exists('Unish\CommandUnishTestCase')) {
       $this->drush('updb', $modules, $options);
       $this->drush('updb', $modules, $options);
 
-      $this->drush('replication-start', array('', ''), array('replicator' => ''));
-      $output = $this->getOutput();
-      $this->assertContains('status', $output, 'Message.');
+      // Test replication-start when source and target parameters are missing.
+      $this->drush('replication-start', array('', ''), array('replicator' => ''), NULL, NULL, self::EXIT_ERROR);
+      $output = $this->getErrorOutput();
+      $this->assertContains('Missing required arguments: source, target.', $output, 'Missing required arguments: source, target.');
+
+      // Test replication-start when source and target sites doesn't exist.
+      $this->drush('replication-start', array('fake_site1', 'fake_site2'), array('replicator' => ''), NULL, NULL, self::EXIT_ERROR);
+      $output = $this->getErrorOutput();
+      $this->assertContains('Source database not found.', $output, 'Source database not found.');
+      $this->assertContains('Target database not found.', $output, 'Target database not found.');
+
+      // Test replication-stop when source and target parameters are missing.
+      $this->drush('replication-stop', array('', ''), array('replicator' => ''), NULL, NULL, self::EXIT_ERROR);
+      $output = $this->getErrorOutput();
+      $this->assertContains('Missing required arguments: source, target.', $output, 'Missing required arguments: source, target.');
+
+      // Test replication-stop when source and target sites doesn't exist.
+      $this->drush('replication-stop', array('fake_site1', 'fake_site2'), array('replicator' => ''), NULL, NULL, self::EXIT_ERROR);
+      $output = $this->getErrorOutput();
+      $this->assertContains('Source database not found.', $output, 'Source database not found.');
+      $this->assertContains('Target database not found.', $output, 'Target database not found.');
+
+      // Test replication-active when the replicator is not specified.
+      $this->drush('replication-active', array('', ''), array('replicator' => ''), NULL, NULL, self::EXIT_ERROR);
+      $output = $this->getErrorOutput();
+      $this->assertContains('Could not connect to server', $output, 'Could not connect to server');
     }
   }
 
