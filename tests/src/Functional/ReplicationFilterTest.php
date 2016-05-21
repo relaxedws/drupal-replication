8ad97ecc514bb31a40fa389f111be980739411b2
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-05-20 19:42:30 -0500

    Add test for published filter

--- tests/src/Functional/ReplicationFilterTest.php
+++ tests/src/Functional/ReplicationFilterTest.php
@@ -68,5 +68,50 @@ class ReplicationFilterTest extends BrowserTestBase {
     $this->assertCount(1, $changes);
   }
 
+  /**
+   * Test filtering published entities.
+   */
+  public function testPublishedFilter() {
+    $container = \Drupal::getContainer();
+    $workspace_manager = $container->get('workspace.manager');
+    $changes_factory = $container->get('replication.changes_factory');
+    $revisiondiff_factory = $container->get('replication.revisiondiff_factory');
+    $replication_filter_manager = $container->get('plugin.manager.replication_filter');
+
+    $workspace = Workspace::create(['machine_name' => 'default', 'type' => 'basic']);
+    $workspace->save();
+
+    $permissions = [
+      'administer nodes',
+      'administer workspaces',
+    ];
+    $user = $this->drupalCreateUser($permissions);
+    $this->drupalLogin($user);
+
+    $node_type = NodeType::create([
+      'type' => 'test',
+      'label' => 'Test',
+    ]);
+    $node_type->save();
+    $node1 = Node::create([
+      'type' => 'test',
+      'title' => 'Test Node 1',
+      'uid' => $user->id(),
+      'status' => NODE_PUBLISHED,
+    ]);
+    $node1->workspace = $workspace;
+    $node1->save();
+    $node2 = Node::create([
+      'type' => 'test',
+      'title' => 'Test Node 2',
+      'uid' => $user->id(),
+      'status' => NODE_NOT_PUBLISHED,
+    ]);
+    $node2->workspace = $workspace;
+    $node2->save();
+
+    $changes = $changes_factory->get($workspace)->filter('published')->getNormal();
+    $this->assertCount(1, $changes, TRUE);
+  }
 }
 
