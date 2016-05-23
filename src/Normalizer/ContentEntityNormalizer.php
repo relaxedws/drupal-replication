60d39c1938aa9402df3b4335b1d4a316b35c840c
Andrei Jechiu <andrei.jechiu@gmail.com>
2016-05-23 17:01:08 +0300

    Add configuration form for users mapping.

--- src/Normalizer/ContentEntityNormalizer.php
+++ src/Normalizer/ContentEntityNormalizer.php
@@ -331,7 +331,7 @@ class ContentEntityNormalizer extends NormalizerBase implements DenormalizerInte
           unset($translations[$default_langcode][$revision_key]);
         }
         $translations[$default_langcode]['status'][0]['value'] = FILE_STATUS_PERMANENT;
-        $translations[$default_langcode]['uid'][0]['target_id'] = \Drupal::config('multiversion.settings')->get('author_id');
+        $translations[$default_langcode]['uid'][0]['target_id'] = \Drupal::config('replication.settings')->get('uid');
         $entity = $storage->create($translations[$default_langcode]);
       }
     }
@@ -432,7 +432,7 @@ class ContentEntityNormalizer extends NormalizerBase implements DenormalizerInte
           // object.
           if ($target_entity_type_id === 'user') {
             $translation[$field_name][$delta] = array(
-              'target_id' => \Drupal::config('multiversion.settings')->get('author_id'),
+              'target_id' => \Drupal::config('replication.settings')->get('uid'),
             );
             unset($item['entity_type_id']);
             unset($item['target_uuid']);
