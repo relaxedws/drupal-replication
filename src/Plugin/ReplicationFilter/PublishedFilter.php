ea218baa28a53fb18f58905e7daacae6afa79c22
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-05-24 15:47:44 -0500

    Modify published filter to only work on nodes

--- src/Plugin/ReplicationFilter/PublishedFilter.php
+++ src/Plugin/ReplicationFilter/PublishedFilter.php
@@ -3,16 +3,19 @@
 namespace Drupal\replication\Plugin\ReplicationFilter;
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\node\NodeInterface;
 use Drupal\replication\Plugin\ReplicationFilter\ReplicationFilterBase;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * Provides a filter for published entities.
  *
+ * Note: this filter will only work with NodeTypes.
+ *
  * @ReplicationFilter(
  *   id = "published",
- *   label = @Translation("Filter Published Entities"),
- *   description = @Translation("Replicate only entities that are published.")
+ *   label = @Translation("Filter Published Nodes"),
+ *   description = @Translation("Replicate only nodes that are published.")
  * )
  */
 class PublishedFilter extends ReplicationFilterBase {
@@ -20,10 +23,11 @@ class PublishedFilter extends ReplicationFilterBase {
   /**
    * {@inheritdoc}
    */
-  public function filter(EntityInterface $entity = NULL, ParameterBag $parameters) {
-    // @todo If entity is NULL, include it?
-    // @todo how to handle non-node entities that don't have an isPublished?
-    return NULL == $entity || $entity->isPublished();
+  public function filter(EntityInterface $entity, ParameterBag $parameters) {
+    if (!$entity instanceof NodeInterface) {
+      return false;
+    }
+    return $entity->isPublished();
   }
 
 }
