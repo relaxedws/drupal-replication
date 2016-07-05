1f0f9f22d9a3355d77a758a49e79f2d5645427a8
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-07-05 13:02:44 -0500

    Add ReplicationSettings config entity

---
+++ src/Entity/ReplicationSettingsInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\replication\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for replication settings entities.
+ */
+interface ReplicationSettingsInterface extends ConfigEntityInterface {
+
+  /**
+   * Get the plugin ID of the replication filter to use.
+   *
+   * @return string
+   */
+  public function getFilterId();
+
+}
