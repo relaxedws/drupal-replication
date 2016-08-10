1cbaf808dbe0e05a5e6bafb9277b461ca537e900
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-08-10 14:13:03 -0500

    Handle all entity types in type filter

--- src/Plugin/ReplicationFilter/PublishedFilter.php
+++ src/Plugin/ReplicationFilter/PublishedFilter.php
@@ -3,8 +3,9 @@
 namespace Drupal\replication\Plugin\ReplicationFilter;
 
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\node\NodeInterface;
 use Drupal\replication\Plugin\ReplicationFilter\ReplicationFilterBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
  * Provides a filter for published entities.
@@ -17,16 +18,47 @@ use Drupal\replication\Plugin\ReplicationFilter\ReplicationFilterBase;
  *   description = @Translation("Replicate only nodes that are published.")
  * )
  */
-class PublishedFilter extends ReplicationFilterBase {
+class PublishedFilter extends ReplicationFilterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new PublishedFilter.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity_type.manager'));
+  }
 
   /**
    * {@inheritdoc}
    */
   public function filter(EntityInterface $entity) {
-    if (!$entity instanceof NodeInterface) {
-      return FALSE;
+    $definition = $this->entityTypeManager->getDefinition($entity->getEntityTypeId());
+    if ($definition->get('status')) {
+      return $entity->status;
     }
-    return $entity->isPublished();
+    // Assume all entities without 'status' are published.
+    return TRUE;
   }
 
 }
