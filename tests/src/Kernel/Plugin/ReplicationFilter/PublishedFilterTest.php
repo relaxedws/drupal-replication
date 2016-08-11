0c512f073cc9f77490982e4f68913456ee885380
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-08-11 10:24:40 -0500

    Add test for published filter's default config

--- tests/src/Kernel/Plugin/ReplicationFilter/PublishedFilterTest.php
+++ tests/src/Kernel/Plugin/ReplicationFilter/PublishedFilterTest.php
@@ -2,13 +2,11 @@
 
 namespace Drupal\Tests\replication\Kernel;
 
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\block_content\Entity\BlockContent;
 use Drupal\block_content\Entity\BlockContentType;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
-use Drupal\replication\Plugin\ReplicationFilter\PublishedFilter;
 
 /**
  * Tests that the published filter parses parameters correctly.
@@ -59,16 +57,16 @@ class PublishedFilterTest extends KernelTestBase {
   /**
    * Test filtering published entities.
    *
-   * @dataProvider filterTestProvider
-   *
    * @param bool $include_unpublisheable_entities
    *   The plugin configuration value for including unpublisheable entities.
    * @param string $entity_class
-   *   The fully qualified class name (FQCN) of the entity to create for testing the filter.
+   *   Fully qualified class name (FQCN) of the entity to create for testing.
    * @param array $entity_values
    *   The values to pass to $class::create().
    * @param bool $expected
    *   The expected return value from the filter function.
+   *
+   * @dataProvider filterTestProvider
    */
   public function testFilter($include_unpublisheable_entities, $entity_class, $entity_values, $expected) {
     /** @var \Drupal\replication\Plugin\ReplicationFilterManagerInterface $filter_manager */
@@ -85,12 +83,30 @@ class PublishedFilterTest extends KernelTestBase {
   }
 
   /**
+   * Test default configuration for published filter.
+   */
+  public function testDefaultConfig($include_unpublisheable_entities, $entity_class, $entity_values, $expected) {
+    /** @var \Drupal\replication\Plugin\ReplicationFilterManagerInterface $filter_manager */
+    $filter_manager = $this->container->get('plugin.manager.replication_filter');
+    $filter = $filter_manager->createInstance('published');
+    $entity = BlockContent::create([
+      'type' => 'test',
+    ]);
+
+    $value = $filter->filter($entity);
+
+    // By default entities with no status entity key are filtered out.
+    $this->assertFalse($value);
+  }
+
+  /**
    * Provide test cases for the "entity_type_id" and "bundle" parameters.
    *
    * Note: the only node bundle is 'test' and the only block content bundle is
    * 'test'.
    *
    * @return array
+   *   An array of arrays, each array being the arguments to filterTest().
    */
   public function filterTestProvider() {
     $published_node = [
@@ -116,4 +132,3 @@ class PublishedFilterTest extends KernelTestBase {
   }
 
 }
-
