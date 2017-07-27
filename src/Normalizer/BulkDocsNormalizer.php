c4c67405720e248c6cb070e39b39ca0543ec98e7
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-07-27 15:05:07 +0300

    Catch exeptions on bulk docs denormalization.

--- src/Normalizer/BulkDocsNormalizer.php
+++ src/Normalizer/BulkDocsNormalizer.php
@@ -32,41 +32,46 @@ class BulkDocsNormalizer extends NormalizerBase implements DenormalizerInterface
       throw new LogicException('A \'workspace\' context is required to denormalize revision diff data.');
     }
 
-    /** @var \Drupal\replication\BulkDocs\BulkDocsInterface $bulk_docs */
-    $bulk_docs = \Drupal::service('replication.bulkdocs_factory')->get($context['workspace']);
+    try {
+      /** @var \Drupal\replication\BulkDocs\BulkDocsInterface $bulk_docs */
+      $bulk_docs = \Drupal::service('replication.bulkdocs_factory')->get($context['workspace']);
 
-    if (
-      (isset($data['new_edits']) && ($data['new_edits']) === FALSE) ||
-      (isset($context['query']['new_edits']) && ($context['query']['new_edits']) === FALSE)
-    ) {
-      $bulk_docs->newEdits(FALSE);
-    }
+      if (
+        (isset($data['new_edits']) && ($data['new_edits']) === FALSE) ||
+        (isset($context['query']['new_edits']) && ($context['query']['new_edits']) === FALSE)
+      ) {
+        $bulk_docs->newEdits(FALSE);
+      }
 
-    $entities = [];
-    if (isset($data['docs'])) {
-      foreach ($data['docs'] as $doc) {
-        if (!empty($doc)) {
-          // @todo {@link https://www.drupal.org/node/2599934 Find a more generic way to denormalize.}
-          if (!empty($doc['_id']) && strpos($doc['_id'], 'local') !== FALSE) {
-            // Denormalize replication_log entities. This is used when the
-            // replication_log entity format is not standard, for example when
-            // replicating content from PouchDB.
-            list($prefix, $entity_uuid) = explode('/', $doc['_id']);
-            if ($prefix == '_local' && $entity_uuid) {
-              $entity = $this->serializer->denormalize($doc, 'Drupal\replication\Entity\ReplicationLog', $format, $context);
+      $entities = [];
+      if (isset($data['docs'])) {
+        foreach ($data['docs'] as $doc) {
+          if (!empty($doc)) {
+            // @todo {@link https://www.drupal.org/node/2599934 Find a more generic way to denormalize.}
+            if (!empty($doc['_id']) && strpos($doc['_id'], 'local') !== FALSE) {
+              // Denormalize replication_log entities. This is used when the
+              // replication_log entity format is not standard, for example when
+              // replicating content from PouchDB.
+              list($prefix, $entity_uuid) = explode('/', $doc['_id']);
+              if ($prefix == '_local' && $entity_uuid) {
+                $entity = $this->serializer->denormalize($doc, 'Drupal\replication\Entity\ReplicationLog', $format, $context);
+              }
             }
-          }
-          // Check if the document is a valid Relaxed format.
-          elseif (isset($doc['@context'])) {
-            $entity = $this->serializer->denormalize($doc, 'Drupal\Core\Entity\ContentEntityInterface', $format, $context);
-          }
-          if (!empty($entity)) {
-            $entities[] = $entity;
+            // Check if the document is a valid Relaxed format.
+            elseif (isset($doc['@context'])) {
+              $entity = $this->serializer->denormalize($doc, 'Drupal\Core\Entity\ContentEntityInterface', $format, $context);
+            }
+            if (!empty($entity)) {
+              $entities[] = $entity;
+            }
           }
         }
       }
+      $bulk_docs->setEntities($entities);
     }
-    $bulk_docs->setEntities($entities);
+    catch (\Exception $e) {
+      watchdog_exception('Replication', $e);
+    }
 
     return $bulk_docs;
   }
