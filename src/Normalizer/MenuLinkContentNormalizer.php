8bc9c8845c5d22985cc9697e974593f7aa5c127f
Andrei Jechiu <andrei.jechiu@gmail.com>
2018-03-26 12:02:16 +0300

    Add menu link content normalizer.

---
+++ src/Normalizer/MenuLinkContentNormalizer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\replication\Normalizer;
+
+use Drupal\menu_link_content\MenuLinkContentInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class MenuLinkContentNormalizer extends ContentEntityNormalizer implements DenormalizerInterface {
+
+  /**
+   * @var string[]
+   */
+  protected $supportedInterfaceOrClass = ['Drupal\menu_link_content\MenuLinkContentInterface'];
+
+  /**
+   * @var string[]
+   */
+  protected $format = ['json'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($data, $format = NULL, array $context = []) {
+    $normalized = parent::normalize($data, $format, $context);
+
+    return $normalized;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function denormalize($data, $class, $format = NULL, array $context = []) {
+    $denormalized = parent::denormalize($data, $class, $format, $context);
+
+    $entity_type_id = 'menu_link_content';
+    $parent = ($denormalized instanceof MenuLinkContentInterface) ? $denormalized->getParentId() : NULL;
+    if (!empty($parent) && strpos($parent, $entity_type_id) === 0) {
+      list($type, $uuid,) = explode(':', $parent);
+      if ($type === 'menu_link_content' && $uuid) {
+        $storage = $this->entityManager->getStorage($entity_type_id);
+        $parent = $storage->loadByProperties(['uuid' => $uuid]);
+        $parent = reset($parent);
+        if ($parent instanceof MenuLinkContentInterface && $parent->id()) {
+          $denormalized->parent->value = $type . ':' . $uuid . ':' . $parent->id();
+        }
+        elseif (!$parent) {
+          // Create a new menu link as stub.
+          $selection_instance = $this->selectionManager->getInstance(['target_type' => $entity_type_id]);
+          $parent = $selection_instance->createNewEntity($entity_type_id, $entity_type_id, rand(), 1);
+          // Indicate that this revision is a stub.
+          $parent->_rev->is_stub = TRUE;
+          $parent->uuid->value = $uuid;
+          $parent->link->value = 'internal:/';
+          $parent->menu->value = $denormalized->getMenuName();
+          $parent->save();
+          $denormalized->parent->value = $type . ':' . $uuid . ':' . $parent->id();
+        }
+      }
+    }
+
+    return $denormalized;
+  }
+
+  public function supportsDenormalization($data, $type, $format = NULL) {
+    if (in_array($type, ['Drupal\menu_link_content\MenuLinkContentInterface', 'Drupal\Core\Entity\ContentEntityInterface'], true)) {
+      if ($data['@type'] == 'menu_link_content') {
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
+
+}
