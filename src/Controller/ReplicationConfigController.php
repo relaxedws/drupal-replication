a50c1fc3fea0680e0ad730d36c0447ada5c1f6a5
Andrei Jechiu <andrei.jechiu@gmail.com>
2019-07-05 12:19:04 +0300

    Move some of the forms to Workspace module, this is better as the Replication et is defined there.

--- src/Controller/ReplicationConfigController.php
+++ src/Controller/ReplicationConfigController.php
@@ -4,14 +4,17 @@ namespace Drupal\replication\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Class ReplicationConfigController.
+ *
+ * @package Drupal\replication\Controller
+ */
 class ReplicationConfigController extends ControllerBase {
 
   /**
    * Returns the replication configuration forms.
    */
   public function getForms() {
-    $build['replication_unblock_button'] = $this->formBuilder()->getForm('Drupal\replication\Form\UnblockReplicationForm');
-    $build['clear_queue_button'] = $this->formBuilder()->getForm('Drupal\replication\Form\ClearReplicationQueueForm');
     $build['replication_settings'] = $this->formBuilder()->getForm('Drupal\replication\Form\SettingsForm');
     return $build;
   }
